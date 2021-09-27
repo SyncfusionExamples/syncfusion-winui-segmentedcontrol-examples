aac72b3c673e1211695743b97387018065e93e87
JagadeesanPichaimuthu <44426969+JagadeesanPichaimuthu@users.noreply.github.com>
2021-09-27 16:24:40 +0530

    Add files via upload

---
+++ Samples/UI-Customization/UI-Customization/ViewModel/SegmentedViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI_Customization
+{
+    public class SegmentedViewModel
+    {
+        public SegmentedViewModel()
+        {
+            Days = new List<SegmentedModel>();
+            Days.Add(new SegmentedModel() { Name = "Day" });
+            Days.Add(new SegmentedModel() { Name = "Week" });
+            Days.Add(new SegmentedModel() { Name = "Month" });
+            Days.Add(new SegmentedModel() { Name = "Year" });
+
+            ShirtModels = new List<SegmentedModel>();
+            ShirtModels.Add(new SegmentedModel() { Name = "Formals" });
+            ShirtModels.Add(new SegmentedModel() { Name = "Casuals" });
+            ShirtModels.Add(new SegmentedModel() { Name = "Trendy" });
+        }
+
+        public List<SegmentedModel> Days
+        {
+            get; set;
+        }
+
+        public List<SegmentedModel> ShirtModels
+        {
+            get; set;
+        }
+    }
+}
