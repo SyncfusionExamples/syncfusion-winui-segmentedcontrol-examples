0056594a83a9a6be79f1bc4b612ece6d223d0020
JagadeesanPichaimuthu <44426969+JagadeesanPichaimuthu@users.noreply.github.com>
2021-09-27 16:19:28 +0530

    Add files via upload
    Added the sample

---
+++ Samples/Circle-Style/Circle-Style/Model/SegmentedModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Style
+{
+    public class SegmentedModel : INotifyPropertyChanged
+    {
+        private Brush background;
+
+        public Brush Background
+        {
+            get { return background; }
+            set { background = value; OnPropertyChanged("Background"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string parameter)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(parameter));
+        }
+    }
+}
---
+++ Samples/Circle-Style/Circle-Style/ViewModel/SegmentedViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Style
+{
+    public class SegmentedViewModel
+    {
+        public SegmentedViewModel()
+        {
+            Colors = new List<SegmentedModel>();
+            Colors.Add(new SegmentedModel() { Background = new SolidColorBrush(Microsoft.UI.Colors.LightGreen) });
+            Colors.Add(new SegmentedModel() { Background = new SolidColorBrush(Microsoft.UI.Colors.Red) });
+            Colors.Add(new SegmentedModel() { Background = new SolidColorBrush(Microsoft.UI.Colors.OrangeRed) });
+            Colors.Add(new SegmentedModel() { Background = new SolidColorBrush(Microsoft.UI.Colors.DarkGreen) });
+            Colors.Add(new SegmentedModel() { Background = new SolidColorBrush(Microsoft.UI.Colors.DarkBlue) });
+            Colors.Add(new SegmentedModel() { Background = new SolidColorBrush(Microsoft.UI.Colors.Maroon) });
+            Colors.Add(new SegmentedModel() { Background = new SolidColorBrush(Microsoft.UI.Colors.DeepSkyBlue) });
+        }
+
+        public List<SegmentedModel> Colors
+        {
+            get; set;
+        }
+    }
+}
