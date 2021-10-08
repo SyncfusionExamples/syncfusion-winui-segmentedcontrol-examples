bd03e4e66513bd09829219fa8aff70f4f52d6805
JagadeesanPichaimuthu <44426969+JagadeesanPichaimuthu@users.noreply.github.com>
2021-10-08 12:37:02 +0530

    Add files via upload

---
+++ Samples/ItemTemplateSelector/UI-Customization/Model/SegmentedModel.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI_Customization
+{
+    public class SegmentedModel : INotifyPropertyChanged
+    {
+        private string name;
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged("Name"); }
+        }
+
+
+        private string icon;
+        public string Icon
+        {
+            get { return icon; }
+            set { icon = value; OnPropertyChanged("Icon"); }
+        }
+
+        private Brush background;
+
+        public Brush Background
+        {
+            get { return background; }
+            set { background = value; OnPropertyChanged("Background"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string parameter)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(parameter));
+        }
+    }
+}
