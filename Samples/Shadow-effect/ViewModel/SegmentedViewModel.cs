1effbe44bea537c1839887e8e0863493c6b5e8b3
AsarudeenAbdulajis <93648754+AsarudeenAbdulajis@users.noreply.github.com>
2021-12-18 20:10:08 +0530

    Add files via upload

---
+++ Samples/Shadow-effect/ViewModel/SegmentedViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shadow_effect
+{
+    public class SegmentedViewModel
+    {
+        public SegmentedViewModel()
+        {
+            ShirtModels = new List<SegmentedModel>();
+            ShirtModels.Add(new SegmentedModel() { Name = "Formals" });
+            ShirtModels.Add(new SegmentedModel() { Name = "Casuals" });
+            ShirtModels.Add(new SegmentedModel() { Name = "Trendy" });
+        }
+
+        public List<SegmentedModel> ShirtModels
+        {
+            get; set;
+        }
+    }
+}
